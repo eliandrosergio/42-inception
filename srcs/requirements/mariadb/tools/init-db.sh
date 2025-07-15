fcbb6499e5a1520624c581acf921e6c689147e91
Eliandro Faustino <efaustin@c3r4s2.42luanda.com>
2025-07-15 10:26:17 +0100

    dia 15 de 07 de 2025 | alter numero 0002

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+
 # Iniciar o MariaDB em background para configuração
 mysqld_safe &
 
 # Aguardar o MariaDB estar pronto
-sleep 5
+echo "Aguardando MariaDB inicializar..."
+until mysqladmin ping -h localhost --silent; do
+    sleep 1
+done
 
-# Configurar usuário root e banco
-mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
-mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pass';"
-mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';"
+echo "MariaDB iniciado, configurando usuários..."
+
+# Configurar usuário root
+mysql -e "UPDATE mysql.user SET Host='%' WHERE User='root' AND Host='localhost';"
+mysql -e "SET PASSWORD FOR 'root'@'%' = PASSWORD('${MYSQL_ROOT_PASSWORD}');"
+
+# Criar banco e usuário
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
+echo "Configuração concluída!"
+
+# Parar o processo em background
+pkill mysqld
+sleep 2
+
 # Manter o MariaDB em foreground
-exec mysqld
+exec mysqld_safe
