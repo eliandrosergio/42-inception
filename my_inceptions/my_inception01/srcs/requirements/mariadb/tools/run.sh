a347558bcb0cfe1fd349cdac8f01a11f81b4e7f6
eliandrosergio <eliandrosergio42@gmail.com>
2025-07-22 21:32:28 +0100

    grande finale | a ultima versao tava lixada

--- my_inceptions/my_inception01/srcs/requirements/mariadb/tools/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sed -i 's|MYSQL_DB|'${MYSQL_DB}'|g' /tmp/init.sql
-sed -i 's|MYSQL_USER|'${MYSQL_USER}'|g' /tmp/init.sql
-sed -i 's|MYSQL_PASS|'${MYSQL_PASS}'|g' /tmp/init.sql
-sed -i 's|MYSQL_RPASS|'${MYSQL_RPASS}'|g' /tmp/init.sql
-
-mysql < /tmp/init.sql
