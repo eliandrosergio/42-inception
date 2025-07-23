12c092f4607e86cf1edb2c541347843c337d3e02
eliandrosergio <eliandrosergio42@gmail.com>
2025-07-23 17:03:27 +0100

    agora estamos quase lá amigo

--- inception_efaustin/srcs/requirements/mariadb/tools/run.sh
+++ inception_efaustin/srcs/requirements/mariadb/tools/run.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
+# Lendo as senhas dos secrets
+DB_PASS=$(cat /run/secrets/db_password)
+DB_RTPASS=$(cat /run/secrets/db_root_password)
+
+# Substituindo as variáveis no arquivo SQL
 sed -i 's|DB_NAME|'${DB_NAME}'|g' /tmp/init.sql
 sed -i 's|DB_USER|'${DB_USER}'|g' /tmp/init.sql
 sed -i 's|DB_PASS|'${DB_PASS}'|g' /tmp/init.sql
 sed -i 's|DB_RTPASS|'${DB_RTPASS}'|g' /tmp/init.sql
 
 if [ -d "/var/lib/mysql/$DB_NAME" ]
-
 then
   echo "✅ Database existente."
   mysqld_safe
-
 else
   mysql_install_db
   mysqld --init-file="/tmp/init.sql"
-
 fi
 
 exec "$@"
