fc3a3abfc68802f6e13c9abdce38ed6fde4ca225
efaustin <efaustin@efaustin.efaustin.inception.org>
2025-08-14 11:35:35 +0100

    update das ultimas etapas

--- inception_efaustin/srcs/requirements/bonus/adminer/tools/script.sh
+++ inception_efaustin/srcs/requirements/bonus/adminer/tools/script.sh
@@ -3,12 +3,17 @@
 mkdir -p /var/www
 mkdir -p /var/www/adminer
 
-wget "http://www.adminer.org/latest.php" -O /var/www/adminer/adminer.php
+if [ -d "/var/www/adminer/adminer.php" ]
+then
+  echo "✅ adminer.php existente."
+else
+  wget "http://www.adminer.org/latest.php" -O /var/www/adminer/adminer.php
+fi
 
 chown -R www-data:www-data /var/www/adminer/adminer.php 
 chmod 755 /var/www/adminer/adminer.php
 
 cd /var/www/adminer
-mv adminer.php index.php
+cp adminer.php index.php
 
 php -S 0.0.0.0:80
--- inception_efaustin/srcs/requirements/nginx/tools/run.sh
+++ inception_efaustin/srcs/requirements/nginx/tools/run.sh
@@ -3,10 +3,11 @@
 mkdir -p /var/www/wordpress
 chown -R www-data /var/www/wordpress
 
-openssl req -x509 -nodes -days 365 -subj \
-"/C=FR/ST=Paris/L=Paris/O=42/OU='${DB_USER}'/CN='${DOMAIN_NAME}'" \
--newkey rsa:2048 -keyout /etc/nginx/ssl/nginx-selfsigned.key \
--out /etc/nginx/ssl/nginx-selfsigned.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+-keyout /etc/nginx/ssl/nginx-selfsigned.key \
+-out /etc/nginx/ssl/nginx-selfsigned.crt \
+-subj "/C=FR/ST=Paris/L=Paris/O=42/OU='${DB_USER}'/CN='${DOMAIN_NAME}'" \
+-addext "subjectAltName = DNS:'${DOMAIN_NAME}', DNS:www.'${DOMAIN_NAME}'"
 
 sed -i 's|DOMAIN_NAME|'${DOMAIN_NAME}'|g' /etc/nginx/sites-available/default.conf
 
